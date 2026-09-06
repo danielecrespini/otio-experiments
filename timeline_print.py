a248d7278ae841d78ea63b31a2d6008ce3a9f080
Daniele <danielecrespini@gmail.com>
2026-09-06 19:30:57 +0100

    Apply review fixes

--- timeline_print.py
+++ timeline_print.py
@@ -1,7 +1,11 @@
+import sys
 import opentimelineio as otio
 
+# Get the OTIO file path from the command line
+path = sys.argv[1]
+
 # Load the OTIO file
-timeline = otio.adapters.read_from_file("/u/dacr/Desktop/test_otio_python/EDL_m0125_previs_editorial_ingest_v003_extracted.otio")
+timeline = otio.adapters.read_from_file(path)
 
 # Get the video track v1 and print the clips
 video_track = timeline.video_tracks()
@@ -11,6 +15,13 @@ if video_track:
 
     for clip in v1:
         if isinstance(clip, otio.schema.Clip):
-            track_range = v1.range_of_child(clip)
-            last_frame = (track_range.end_time_exclusive() - otio.opentime.RationalTime(1, track_range.duration.rate))
-            print(clip.name , "- Timecode in:" , otio.opentime.to_timecode(clip.range_in_parent().start_time, rate = 24) , "- Timecode out:" ,  last_frame.to_timecode() , "- Source:" , clip.source_range)
+            range_in_track = v1.range_of_child(clip)
+            last_frame = (
+                range_in_track.end_time_exclusive())
+
+            print( 
+                f"{clip.name} " 
+                f"- Timecode in: {range_in_track.start_time.to_timecode(rate=24)} " 
+                f"- Timecode out: {last_frame.to_timecode()} " 
+                f"- Source: {clip.source_range}" 
+            )
