27d50e10e37b29a75b8d44fbec616009e48d9ff5
danielecrespini <danielecrespini@gmail.com>
2026-09-08 19:47:06 +0100

    Add main function

--- timeline_print.py
+++ timeline_print.py
@@ -1,32 +1,38 @@
 import sys
 import opentimelineio as otio
 
-# Get the OTIO file path from the command line
-path = sys.argv[1]
 
-# Load the OTIO file
-timeline = otio.adapters.read_from_file(path)
+def main():
+    # Get the OTIO file path from the command line
+    path = sys.argv[1]
 
-# Get the video track v1 and print the clips
-video_tracks = timeline.video_tracks() #video_tracks because is a list of tracks
+    # Load the OTIO file
+    timeline = otio.adapters.read_from_file(path)
 
-if not video_tracks:
-    print(f"No valid video tracks in {path}")
-    sys.exit()
+    # Get the video track v1 and print the clips
+    video_tracks = timeline.video_tracks() #video_tracks because is a list of tracks
 
-v1 = video_tracks[0]
+    if not video_tracks:
+        print(f"No valid video tracks in {path}")
+        sys.exit()
 
-for clip in v1:
-    if not isinstance(clip, otio.schema.Clip):
-        continue
+    v1 = video_tracks[0]
 
-    range_in_track = v1.range_of_child(clip)
-    last_frame = (
-        range_in_track.end_time_exclusive())
+    for clip in v1:
+        if not isinstance(clip, otio.schema.Clip):
+            continue
 
-    print( 
-        f"{clip.name} " 
-        f"- Timecode in: {range_in_track.start_time.to_timecode()} " 
-        f"- Timecode out: {last_frame.to_timecode()} " 
-        f"- Source: {clip.source_range}" 
-    )
+        range_in_track = v1.range_of_child(clip)
+        last_frame = (
+            range_in_track.end_time_exclusive())
+
+        print( 
+            f"{clip.name} " 
+            f"- Timecode in: {range_in_track.start_time.to_timecode()} " 
+            f"- Timecode out: {last_frame.to_timecode()} " 
+            f"- Source: {clip.source_range}" 
+        )
+
+
+if __name__=="__main__":
+    main()
